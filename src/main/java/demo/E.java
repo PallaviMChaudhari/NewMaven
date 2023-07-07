7772866e4b06abc3fcdb6ce92ce1e9faf51c0cb7
vishaldongare2907 <dongarevishalias2907@gmail.com>
2023-07-07 18:45:55 +0530

    [Vishal Dongare]making changes.

--- src/main/java/demo/E.java
+++ src/main/java/demo/E.java
@@ -2,8 +2,9 @@ package demo;
 
 public class E {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Punes monsoon is very awesome");
+		Thread.sleep(1000);
 
 	}
 
